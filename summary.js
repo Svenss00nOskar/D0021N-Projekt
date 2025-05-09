8fa878aa3db1a43274d94ae8bc09f8211379a783
Svenss00nOskar <150705008+Svenss00nOskar@users.noreply.github.com>
2025-05-09 14:08:13 +0200

    Summering fixad
    Gjorde så summeringssidan inte listas och lite förklarande text kom.

--- summary.js
+++ summary.js
@@ -4,9 +4,11 @@ let visitedPages = JSON.parse(localStorage.getItem('visitedPages')) || [];
 // Lägg till den aktuella sidans titel eller URL om den inte redan finns
 const currentPage = document.title || window.location.pathname;
 
-if (!visitedPages.includes(currentPage)) {
-  visitedPages.push(currentPage);
-  localStorage.setItem('visitedPages', JSON.stringify(visitedPages));
+if (currentPage != "Besökssummering") {
+  if (!visitedPages.includes(currentPage)) {
+    visitedPages.push(currentPage);
+    localStorage.setItem('visitedPages', JSON.stringify(visitedPages));
+  }
 }
 
 // Funktion för att hämta besökssammanfattning (om du vill använda)
