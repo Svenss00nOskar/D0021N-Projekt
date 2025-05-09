1aa5a05d2ebd2f43ed9129f0acfbcf82697d6e47
Margarita Z <marzve-3@student.ltu.se>
2025-05-09 11:07:02 +0200

    fix av responsiv layout samt personlig summering-knappen

---
+++ hamburger-menu.js
@@ -0,0 +1,15 @@
+function showMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+  
+    if (hamburger && navLinks) {
+      hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+      });
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    showMenu();
+  });
+  
